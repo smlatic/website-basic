9c7f738dbc57bbbf656fb854baa5e8421e46da79
smlatic <107187154+smlatic@users.noreply.github.com>
2022-11-26 14:13:53 +0100

    sidebar multipage test

---
+++ pages/1_projects.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title("Projects")
+
+
+# --- Sidebar ---
+with st.sidebar:
+    st.header("Header title")
+    st.subheader("This is the sub-header")
+    st.write("asdklasdasdlas ")
